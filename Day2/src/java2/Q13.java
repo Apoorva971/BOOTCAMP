233d1fe3bfe503d7fd0beb2753f07ac00e30275c
Apoorva971 <apoorva.garg@tothenew.com>
2020-02-19 22:54:32 +0530

    java2-assignment

---
+++ Day2/src/java2/Q13.java
@@ -0,0 +1,17 @@
+package java2;
+
+public class Q13 {
+        public static void main(String[] args) {
+            try {
+                throw new CustomException("custom exception");
+            } catch (CustomException e){
+                System.out.println(e.getMessage());
+            }
+        }
+    }
+
+    class CustomException extends Exception{
+        CustomException(String message){
+            super(message);
+        }
+    }
