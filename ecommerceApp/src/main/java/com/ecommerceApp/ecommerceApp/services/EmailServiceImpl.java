3241e4e9450c1cebf0a77e4fa05def1327c2bdc6
Apoorva971 <apoorva.garg@tothenew.com>
2020-04-14 16:47:37 +0530

    e-commerce

--- ecommerceApp/src/main/java/com/ecommerceApp/ecommerceApp/services/EmailServiceImpl.java
+++ ecommerceApp/src/main/java/com/ecommerceApp/ecommerceApp/services/EmailServiceImpl.java
@@ -6,16 +6,16 @@ import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.scheduling.annotation.Async;
 
 public class EmailServiceImpl {
-    @Autowired
-    public JavaMailSender emailSender;
-
-    @Async
-    public void sendSimpleMessage(String to, String subject, String text) {
-        SimpleMailMessage message = new SimpleMailMessage();
-        message.setTo(to);
-        message.setSubject(subject);
-        message.setText(text);
-        emailSender.send(message);
-    }
+//    @Autowired
+//    public JavaMailSender emailSender;
+//
+//    @Async
+//    public void sendSimpleMessage(String to, String subject, String text) {
+//        SimpleMailMessage message = new SimpleMailMessage();
+//        message.setTo(to);
+//        message.setSubject(subject);
+//        message.setText(text);
+//        emailSender.send(message);
+//    }
 }
 
--- ecommerceApp/src/main/java/com/ecommerceApp/ecommerceApp/services/Emailconfig.java
+++ ecommerceApp/src/main/java/com/ecommerceApp/ecommerceApp/services/Emailconfig.java
@@ -9,24 +9,24 @@ import java.util.Properties;
 
 @Configuration
 public class Emailconfig {
-    @Bean
-    public JavaMailSender getJavaMailSender() {
-        JavaMailSenderImpl mailSender = new JavaMailSenderImpl();
-
-
-        mailSender.setHost("smtp.gmail.com");
-        mailSender.setPort(587);
-
-        mailSender.setUsername("apoorvagarg0@gmail.com");
-        mailSender.setPassword("apoorvagarg");
-
-        Properties properties = mailSender.getJavaMailProperties();
-        properties.put("mail.transport.protocol", "smtp");
-        properties.put("mail.smtp.auth", "true");
-        properties.put("mail.smtp.starttls.enable", "true");
-        properties.put("mail.debug", "true");
-
-        return mailSender;
-    }
+//    @Bean
+//    public JavaMailSender getJavaMailSender() {
+//        JavaMailSenderImpl mailSender = new JavaMailSenderImpl();
+//
+//
+//        mailSender.setHost("smtp.gmail.com");
+//        mailSender.setPort(587);
+//
+//        mailSender.setUsername("apoorvagarg0@gmail.com");
+//        mailSender.setPassword("apoorvagarg");
+//
+//        Properties properties = mailSender.getJavaMailProperties();
+//        properties.put("mail.transport.protocol", "smtp");
+//        properties.put("mail.smtp.auth", "true");
+//        properties.put("mail.smtp.starttls.enable", "true");
+//        properties.put("mail.debug", "true");
+//
+//        return mailSender;
+//    }
 }
 
